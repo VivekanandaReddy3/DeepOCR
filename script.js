e046112ed65874e66e602e8c697078092408fe45
VivekanandaReddy3 <godalavivekananda.in@gmail.com>
2023-12-02 19:57:27 +0530

    Added a Landing Page

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // write your js code here
+var bars = document.getElementById("nav-action");
+var nav = document.getElementById("nav");
+
+
+//setting up the listener
+bars.addEventListener("click", barClicked, false);
+
+
+//setting up the clicked Effect
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
